a1aaa49a71a2ffb09d569c99b6600eec0288fc7d
Simon Anreiter <simon.anreiter@wegfinder.at>
2022-04-25 11:34:24 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PackageUtils",
+    products: [
+        .plugin(
+            name: "CreateArtifactBundle",
+            targets: ["CreateArtifactBundle"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .plugin(
+            name: "CreateArtifactBundle",
+            capability: .command(
+                intent: .custom(
+                    verb: "create-artifact-bundle",
+                    description: "Creates a .zip containing executable products"
+                )
+            )
+        )
+
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# PackageUtils
+
+A description of this package.
